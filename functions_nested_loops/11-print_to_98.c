e70f3215a72ff61a6aff85a55b33714b3ddfa6d8
aishalieva0 <aysalieva6@gmal.com>
2023-10-17 06:54:30 -0700

    11th task: prints all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+		_putchar('\n');
+	}
+}
