4c8d1104386d348e4aedd545c416b87ff2fc4002
aishalieva0 <aysalieva6@gmal.com>
2023-10-17 05:20:07 -0700

    10th task:add two integer

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - adds two integer
+ * @a: first number
+ * @b: second number
+ *
+ * Return: void
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
