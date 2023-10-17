888709b0c57cf0bbd2842dc52dbc01ba4683a1b6
aishalieva0 <aysalieva6@gmal.com>
2023-10-17 02:37:12 -0700

    7th task: prints last digit of number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabet
+ *@n: number which found last digit
+ * Return: void
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	if (n < 0)
+	{
+		lastDigit = -1 * n % 10;
+	}
+	else
+	{
+		lastDigit = n % 10;
+	}
+	_putchar('0' + lastDigit);
+
+	return (lastDigit);
+}
