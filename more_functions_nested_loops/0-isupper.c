d34a63ffc9af19655cfc6446ac39255dfcb9af09
aishalieva0 <aysalieva6@gmal.com>
2023-10-20 10:11:54 -0700

    1st task:checks for uppercase character

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: is number
+ *
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
