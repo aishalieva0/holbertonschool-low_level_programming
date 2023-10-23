70400bab4217c8dd342adb45813bb4de37b9b7b2
aishalieva0 <aysalieva6@gmal.com>
2023-10-23 01:10:27 -0700

    2nd task:the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen -  returns the length of a string
+ * @s: char
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
