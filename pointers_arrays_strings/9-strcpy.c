3b89d285e087ae14d5fd9a765adba06be6305ff6
aishalieva0 <aysalieva6@gmal.com>
2023-10-23 04:34:47 -0700

    9th task

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: string
+ * @src: string
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = _strlen(src);
+	int i;
+
+	for (i = 0; i < length && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < length; i++)
+		dest[i] = '\0';
+	return (dest);
+}
+
