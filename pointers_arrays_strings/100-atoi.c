1b004d072c251ccec720c664ceb020c52855eee9
aishalieva0 <aysalieva6@gmal.com>
2023-10-24 11:28:33 -0700

    10th task

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int num;
+	int sign = 1;
+	int i = 0;
+	int result = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+			i++;
+	}
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = s[i] - '0';
+			result = result * 10 + num;
+		}
+		else
+		{
+			if (result != 0)
+			{
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (result * sign);
+}
