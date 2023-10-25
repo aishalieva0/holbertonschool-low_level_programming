e67af6b359a74ceff58b0c77ddae0cc025ade70d
aishalieva0 <aysalieva6@gmal.com>
2023-10-25 06:58:38 -0700

    2nd task:error solved

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	int src_len = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
+	while (src[i++])
+		src_len++;
+
+	for (i = 0; src[i] && i < n; i++)
 		dest[i] = src[i];
-	}
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
