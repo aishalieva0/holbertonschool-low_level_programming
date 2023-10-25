0333bc84f3a5966e3ceaf8042d964dd415571bea
aishalieva0 <aysalieva6@gmal.com>
2023-10-25 05:45:42 -0700

    2n task:error fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int len = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[len++] = src[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
