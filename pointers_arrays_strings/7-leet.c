15bf678cd662276ad90c73213aad054edc464776
aishalieva0 <aysalieva6@gmal.com>
2023-10-27 13:04:28 -0700

    7th task:encode 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -10,16 +10,12 @@ char *leet(char *str)
 {
 	char *s = str;
 	int i = 0;
-	int len = 0;
 	char letters[] = {'a', 'e', 'o', 't', 'l'};
 	char replace[] = {4, 3, 0, 7, 1};
 
-	while (letters[i++])
-		len++;
-
 	while (*str)
 	{
-		for (i = 0; i < len; i++)
+		for (i = 0; i < 5; i++)
 		{
 			if (*str == letters[i] || *str == letters[i] - 32)
 				*str = replace[i] + '0';
