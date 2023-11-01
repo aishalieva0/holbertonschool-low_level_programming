ae1e58dd04d44bed3c92af27de1722c88bdc2179
aishalieva0 <aysalieva6@gmal.com>
2023-11-01 03:40:26 -0700

    task 1:print number of argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: size of argv
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d \n", argc - 1);
+
+	return (0);
+}
