f9255b3a00d695f4fff6bad1f8e360d0afe60e4e
aishalieva0 <aysalieva6@gmal.com>
2023-11-01 07:48:40 -0700

    task 5:error fixed

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -11,38 +11,42 @@
 int main(int argc, char *argv[])
 {
 	int count = 0;
-	int cents = atoi(argv[1]);
+	int cents = 0;
 
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	while (cents > 0)
-	{
+		cents = atoi(argv[1]);
+		while (cents > 0)
+		{
 			count++;
-			if (cents - 25 >= 0)
+			if ((cents - 25) >= 0)
 			{
 				cents -= 25;
 				continue;
 			}
-			if (cents - 10 >= 0)
+			if ((cents - 10) >= 0)
 			{
 				cents -= 10;
 				continue;
 			}
-			if (cents - 5 >= 0)
+			if ((cents - 5) >= 0)
 			{
 				cents -= 5;
 				continue;
 			}
-			if (cents - 2 >= 0)
+			if ((cents - 2) >= 0)
 			{
 				cents -= 2;
 				continue;
 			}
 			cents--;
-	}
+		}
 	printf("%d\n", count);
 	return (0);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
 }
