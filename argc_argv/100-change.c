1fefc24a2220939ff877d280c7dcdd97a1510c53
aishalieva0 <aysalieva6@gmal.com>
2023-11-01 07:19:47 -0700

    task 5:min number of coin

---
+++ argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+	int cents = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (cents > 0)
+	{
+			count++;
+			if (cents - 25 >= 0)
+			{
+				cents -= 25;
+				continue;
+			}
+			if (cents - 10 >= 0)
+			{
+				cents -= 10;
+				continue;
+			}
+			if (cents - 5 >= 0)
+			{
+				cents -= 5;
+				continue;
+			}
+			if (cents - 2 >= 0)
+			{
+				cents -= 2;
+				continue;
+			}
+			cents--;
+	}
+	printf("%d\n", count);
+	return (0);
+}
