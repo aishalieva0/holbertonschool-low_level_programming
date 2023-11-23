57a23e343dc4aca5cf03d5172525fed140fdb047
aishalieva0 <aysalieva6@gmal.com>
2023-11-23 04:43:02 -0800

    task 0:define stuct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
