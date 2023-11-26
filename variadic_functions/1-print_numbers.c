c5f09b9abb60a71e20516075b7ec213a55e96ec5
aishalieva0 <aysalieva6@gmal.com>
2023-11-26 09:32:19 -0800

    task 1:print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,13 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
-
 	for (j = 0; j < n; j++)
 	{
 		printf("%d", va_arg(args, int));
-		if (j != n - 1)
+		if (j != n - 1 || separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
