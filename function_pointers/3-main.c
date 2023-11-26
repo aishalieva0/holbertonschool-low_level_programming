0d6fb6bbe64c8c894e3ff1afb49970ac58d8dc81
aishalieva0 <aysalieva6@gmal.com>
2023-11-26 12:23:01 -0800

    task

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,29 +2,36 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, other values on failure
+ */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return 98;
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        return 99;
-    }
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return 0;
+	return (0);
 }
