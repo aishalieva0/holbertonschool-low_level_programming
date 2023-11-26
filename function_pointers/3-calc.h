bdd1410a327de6231e4ae88bcbb2401cb65930fd
aishalieva0 <aysalieva6@gmal.com>
2023-11-26 12:02:18 -0800

    tas 3l

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,12 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
-
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#ifndef _3_CALC_H_
+#define _3_CALC_H_
 
 /**
  * struct op - Struct op
@@ -16,8 +9,17 @@ int op_mod(int a, int b);
  */
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
 
-#endif
+/* Function prototypes */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+#endif /* _3_CALC_H_ */
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,53 +1,26 @@
 #include "3-calc.h"
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-/**
- * op_add - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
- * op_sub - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+
+    int i = 0;
+    while (ops[i].op != NULL)
+    {
+        if (*(ops[i].op) == *s && s[1] == '\0')
+            return ops[i].f;
+        i++;
+    }
+
+    printf("Error\n");
+    exit(99);
 }
