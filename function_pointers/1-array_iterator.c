137399e6c52ac9ced14e6ff23119b7a6e6a1a7f4
aishalieva0 <aysalieva6@gmal.com>
2023-11-26 05:28:59 -0800

    task 1:print array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int j;
 
-	if (action != NULL)
+	if (action != NULL && array != NULL)
 		for (j = 0; j < size; j++)
 			action(array[j]);
 }
